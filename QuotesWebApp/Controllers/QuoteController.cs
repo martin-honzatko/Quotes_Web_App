9a1ce2e8a666ebae8cb21d5c71c641324cb8a412
Martin Honzátko <u17.405@pslib.cloud>
2020-10-01 14:20:13 +0200

    API-Init

---
+++ QuotesWebApp/Controllers/QuoteController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using QuotesWebApp.Data;
+
+namespace QuotesWebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuoteController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public QuoteController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Quote
+        [HttpGet]
+        public async Task<ActionResult<Quote>> GetQuote()
+        {
+            var r = new Random().Next(1, 100);
+            var q = await _context.Quotes.SingleOrDefaultAsync(q => q.Id == r);
+
+            if (q != null)
+            {
+                return q;
+            }
+
+            else
+            {
+                r = new Random().Next(1, 100);
+            }
+
+            return StatusCode(418);
+        }
+
+        // GET: api/Quote/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Quote>> GetQuote(int id)
+        {
+            var quote = await _context.Quotes.FindAsync(id);
+
+            if (quote == null)
+            {
+                return NotFound();
+            }
+
+            return quote;
+        }
+
+        // PUT: api/Quote/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutQuote(int id, Quote quote)
+        {
+            if (id != quote.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(quote).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!QuoteExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Quote
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Quote>> PostQuote(Quote quote)
+        {
+            _context.Quotes.Add(quote);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetQuote", new { id = quote.Id }, quote);
+        }
+
+        // DELETE: api/Quote/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Quote>> DeleteQuote(int id)
+        {
+            var quote = await _context.Quotes.FindAsync(id);
+            if (quote == null)
+            {
+                return NotFound();
+            }
+
+            _context.Quotes.Remove(quote);
+            await _context.SaveChangesAsync();
+
+            return quote;
+        }
+
+        private bool QuoteExists(int id)
+        {
+            return _context.Quotes.Any(e => e.Id == id);
+        }
+    }
+}
